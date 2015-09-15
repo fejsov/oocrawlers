1bc7ccb48309f0e1b6e551b9895e47c5d4782398
Dan O'Huiginn <daniel@ohuiginn.net>
2015-09-15 14:41:08 +0200

    improvements to example crawler

--- oocrawlers/examplecrawler.py
+++ oocrawlers/examplecrawler.py
@@ -1,5 +1,32 @@
+import pprint
 
-from aleph.crawlers import Crawler, TagExists
+try:
+    from aleph.crawlers import Crawler, TagExists
+except ImportError:
+
+    # Stubbed-out versions of the Aleph base crawler
+    # Intended only for dev/testing in isolation
+    
+    class TagExists(Exception): pass
+
+    class TestCrawler(object):
+        MAX_RESULTS = 10
+
+        def __init__(self):
+            self.results = []
+
+        def emit_url(self, url, package_id=None, **kwargs):
+            pprint.pprint([url, kwargs])
+            self.results.append([url, package_id, kwargs])
+            if len(self.results) >= self.MAX_RESULTS:
+                raise RuntimeError('finished downloading some items')
+
+        def check_tag(self, *args, **kwargs):
+            return
+
+    Crawler = TestCrawler
+
+    
 
 class ExampleCrawler(Crawler):
 
@@ -25,41 +52,47 @@ class ExampleCrawler(Crawler):
             ]
 
 
-    def announcements_for_company(company_name):
+    def announcements_for_company(self, company_name):
         ## Get all the announcements for the company from 
         return [ #list of [url, anouncement_metadata] 
-['http://infopub.sgx.com/Apps?A=COW_CorpAnnouncement_Content&B=AnnouncementLast12MonthsSecurity&F=D2M3PWUVM5TG6F26&H=96bbc8719840ba378779b2987f27f37452158ff15d8b7cb79a7b34927db58945', {}],
+['http://infopub.sgx.com/Apps?A=COW_CorpAnnouncement_Content&B=AnnouncementLast12MonthsSecurity&F=D2M3PWUVM5TG6F26&H=96bbc8719840ba378779b2987f27f37452158ff15d8b7cb79a7b34927db58945', {'Announcement Title': 'Financial Statements and Related Announcement::Full Yearly Results'}],
                 #...
         ]
     
     def crawl(self):
         for company_name, basic_company_data in self.list_companies():
-            for (announcement_url, announcement_metadata) in announcements_for_company(company_name):
-                attachment_url, detailed_metadata = get_detailed_metadata(announcement_url)
+            for (announcement_url, announcement_metadata) in self.announcements_for_company(company_name):
+                attachment_urls, detailed_metadata = self.get_detailed_metadata(announcement_url)
                 detailed_metadata.update(announcement_metadata)
                 detailed_metadata.update(basic_company_data)
 
-                try:
-                    # Here we check that our datastore does not already
-                    # contain a document with this URL
-                    # Doing so enables us to re-run the scraper without
-                    # filling the datastore with duplicates
-                    
-                    id = self.check_tag(url=url)
+                for attachment_url in attachment_urls:
 
-                    # This is the line that triggers the import into our system
-                    # Aleph will then download the url, store a copy,
-                    # extract the text from it (doing OCR etc as needed)
-                    # and index text, title and metadata
-                    self.emit_url(
-                        url = attachment_url,
-                        title = announcement_metadata['Announcement Title'],
-                        meta = detailed_metadata,
-                    )
+                    try:
+                        # Here we check that our datastore does not already
+                        # contain a document with this URL
+                        # Doing so enables us to re-run the scraper without
+                        # filling the datastore with duplicates
 
-                except TagExists:
-                    pass
+                        id = self.check_tag(url=attachment_url)
+
+                        # This is the line that triggers the import into our system
+                        # Aleph will then download the url, store a copy,
+                        # extract the text from it (doing OCR etc as needed)
+                        # and index text, title and metadata
+                        self.emit_url(
+                            url = attachment_url,
+                            title = announcement_metadata['Announcement Title'],
+                            meta = detailed_metadata,
+                        )
+
+                    except TagExists:
+                        pass
 
                 
 
  
+if __name__ == '__main__':
+    crawler = ExampleCrawler()
+    crawler.crawl()
+    
